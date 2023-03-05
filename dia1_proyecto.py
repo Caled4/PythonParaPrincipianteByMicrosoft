9c0c88eb2ac42ab3438cd85c212add2a97dbf764
Caled4 <7810907062@untrm.edu.pe>
2023-03-05 15:43:24 -0500

    Lo que no subi en su dia

---
+++ dia1_proyecto.py
@@ -0,0 +1,9 @@
+# Vas a crear un código en Python que le pida a tu amigo que responda dos preguntas que requieran una
+# sola palabra cada una y que luego le muestre en pantalla esas palabras combinadas.
+
+print("El nombre de tu cerveza\nes '" + input("Que ciudad te gustaria visitar?: ") + " " + input("Cual es tu color favorito?: ") + "'\nFelicitaciones!")
+
+
+
+
+
