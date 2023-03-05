71cf493755702d1fbd0b0874813f5faa470a8f2f
Caled4 <7810907062@untrm.edu.pe>
2023-03-05 15:39:11 -0500

    Lo que no subi en su dia

--- dia1.py
+++ dia1.py
@@ -1,6 +1,62 @@
 # Práctica Print 1
 # Crea un código que imprima en pantalla la expresión Me encanta estudiar Python.
-
+# ----------------------------------------------------------------------------------------
 print(" Me encanta estudiar Python")
-
-        
+# ----------------------------------------------------------------------------------------
+# Práctica Print 2
+# Crea un código que imprima en pantalla la expresión Estudiar con "Python Total" es super divertido
+# ----------------------------------------------------------------------------------------
+print('Estudiar con "Python Total" es super divertido')
+# ----------------------------------------------------------------------------------------
+# Práctica Print 3
+# Crea un código que imprima en pantalla el número 555, pero no debes imprimirlo directamente, sino como resultado de una operación matemática
+# ----------------------------------------------------------------------------------------
+print(555)
+# ----------------------------------------------------------------------------------------
+# Práctica String 1
+# Crea un código que imprima en pantalla la siguiente expresión (pero usando print una sola vez):
+#     Línea 1
+#     Línea 2
+#     Línea 3
+# ----------------------------------------------------------------------------------------
+print('Línea 1\nLínea 2\nLínea 3')
+# ----------------------------------------------------------------------------------------
+# Práctica String 2
+# Crea un código que imprima en pantalla la siguiente expresión.
+#     A	    B	C
+#     D 	E	F
+#     G 	H	I
+# Notas:
+# - Observa que la distancia horizontal entre los números ha sido creada con UNA tabulación.
+# - Puedes usar una o más declaraciones print
+# ----------------------------------------------------------------------------------------
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+# ----------------------------------------------------------------------------------------
+# Práctica String 3
+# Crea un código que imprima en pantalla la siguiente expresión:
+#     Barra Normal: /
+#     Barra Invertida: \
+# ----------------------------------------------------------------------------------------
+print("Barra Normal: /\nBarra Invertida: \\")
+# ----------------------------------------------------------------------------------------
+# Práctica Input 1
+# Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+# ¿Qué estás estudiando?
+# El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+# ----------------------------------------------------------------------------------------
+input("¿Qué estás estudiando?\n")
+# ----------------------------------------------------------------------------------------
+# Práctica Input 2
+# Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+# ¿En qué país vives?
+# El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+# ----------------------------------------------------------------------------------------
+input("¿En qué país vives?\n")
+# ----------------------------------------------------------------------------------------
+# Práctica Input 3
+# Crea un código que muestre en pantalla el nombre completo del usuario, permitiéndole ingresar su nombre y apellido con las siguientes instrucciones:
+#     Escribe tu nombre:
+#     Escribe tu apellido:
+# El código debe poder imprimir en pantalla el nombre y apellido del usuario, separados por un espacio.
+# ----------------------------------------------------------------------------------------
+print(input("Escribe tu nombre:") + " " + input("Escribe tu apellido:"))
